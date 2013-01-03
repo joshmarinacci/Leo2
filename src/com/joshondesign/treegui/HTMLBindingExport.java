9dea620092152a9fcb44a5fc4cd59918f4064962
Josh Marinacci <joshua@marinacci.org>
2013-01-03 13:51:26 -0800

    improve html export

--- src/com/joshondesign/treegui/HTMLBindingExport.java
+++ src/com/joshondesign/treegui/HTMLBindingExport.java
@@ -7,8 +7,8 @@ import com.joshondesign.treegui.docmodel.ResizableRectNode;
 import com.joshondesign.treegui.docmodel.SketchNode;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import org.joshy.gfx.util.OSUtil;
 
 public class HTMLBindingExport extends JAction {
@@ -24,6 +24,7 @@ public class HTMLBindingExport extends JAction {
             //file.deleteOnExit();
 
             PrintWriter out = new PrintWriter(new FileWriter(file));
+            //PrintWriter out = new PrintWriter(System.out);
             out.println("<html><head>");
             out.println("<script src='amino.js'></script>");
             out.println("</head>");
@@ -34,28 +35,46 @@ public class HTMLBindingExport extends JAction {
 
             out.println("var engine = new Amino();");
             out.println("var root = engine.addCanvas('canvas');");
+            PropWriter w = new PropWriter(out);
             for(Layer layer : page.children()) {
+                w.p("//layer");
+                w.indent();
                 for(SketchNode node : layer.children()) {
-                    out.println("var "+node.getId()+" = ");
-                    out.println("  new Transform(");
+                    w.newObjVarOpen(node.getId(), "Transform").indent();
                     if(node instanceof ResizableRectNode) {
                         ResizableRectNode rect = (ResizableRectNode) node;
-                        out.println("  new Rect().set(0,0,"+rect.getWidth()+","+rect.getHeight()+").setFill('red')");
+                        w.newObj("Rect")
+                            .indent()
+                            .p(".set(0,0," + rect.getWidth() + "," + rect.getHeight() + ")")
+                            .prop("fill", "red")
+                            .outdent();
                     } else {
-                        out.println("  new Rect().set(0,0,100,100).setFill('red')");
+                        w.newObj("Rect")
+                            .indent()
+                            .p(".set(0,0,100,100)")
+                            .prop("fill", "red")
+                            .outdent();
                     }
-                    out.println("  )");
-                    out.println("  .setTranslateX("+node.getTranslateX()+")");
-                    out.println("  .setTranslateY("+node.getTranslateY()+")");
-                    out.println("  ;");
-                    out.println("root.add("+node.getId()+");");
+                    w.newObjVarClose();
+
+                    w.p("translateX", node.getTranslateX());
+                    w.p("translateY", node.getTranslateY());
+                    w.p(";").outdent();
+
+                    w.p("root.add(" + node.getId() + ");");
 
                     if(PropUtils.propertyEquals(node,"draggable",true)) {
-                        out.println("root.onPress("+node.getId()+",function() {\n" +
-                                node.getId()+".setTranslateX("+node.getId()+".getTranslateX()+10);\n"+
-                                "});\n");
+                        w.p("root.onPress("
+                                +node.getId()
+                                +",function() {" +
+                                node.getId()
+                                +".setTranslateX("
+                                +node.getId()
+                                +".getTranslateX()+10);"+
+                                "});");
                     }
                 }
+                w.outdent();
             }
 
 
@@ -74,11 +93,12 @@ public class HTMLBindingExport extends JAction {
             out.println("</script>");
             out.println("</body>");
             out.println("</html>");
+            out.flush();
             //http://projects.joshy.org/Amino3/1.1/amino.js
             out.close();
             //OSUtil.openBrowser(file.toURI().toURL().toString());
             OSUtil.openBrowser("http://localhost/~josh/projects/Leo/t2/foo.html");
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -86,5 +106,85 @@ public class HTMLBindingExport extends JAction {
     @Override
     public String getShortName() {
         return "Export HTML";
+    }
+
+    private class PropWriter {
+        private final PrintWriter out;
+        DecimalFormat decimalFormat = (DecimalFormat) DecimalFormat.getInstance();
+        private int indent;
+
+        public PropWriter(PrintWriter out) {
+            this.out = out;
+            decimalFormat.setMaximumFractionDigits(2);
+            decimalFormat.setMinimumFractionDigits(0);
+        }
+
+        public PropWriter prop(String name, String value) {
+            pindent();
+            out.print(".set" + name.substring(0, 1).toUpperCase() + name.substring(1));
+            out.print("("+"'"+value+"'"+")");
+            out.println();
+            return this;
+        }
+
+        private void pindent() {
+            for(int i=0; i<indent; i++) {
+                out.print("    ");
+            }
+        }
+
+        public PropWriter p(String name, double value) {
+            pindent();
+            out.print(".set" + name.substring(0, 1).toUpperCase() + name.substring(1));
+            out.print("("+""+value+""+")");
+            out.println();
+            return this;
+        }
+
+        public PropWriter indent() {
+            this.indent++;
+            return this;
+        }
+
+        public PropWriter outdent() {
+            this.indent--;
+            return this;
+        }
+
+        public PropWriter pwen() {
+            pindent();
+            out.println(")");
+            return this;
+        }
+
+        public PropWriter pnew(String name) {
+            pindent();
+            out.println(" new "+name+"(");
+            return this;
+        }
+
+        public PropWriter p(String s) {
+            pindent();
+            out.println(s);
+            return this;
+        }
+
+        public PropWriter newObj(String name) {
+            pindent();
+            out.println(" new "+name+"()");
+            return this;
+        }
+
+        public PropWriter newObjVarOpen(String id, String name) {
+            pindent();
+            out.println("var " + id + " = new " + name + "(");
+            return this;
+        }
+
+        public PropWriter newObjVarClose() {
+            pindent();
+            out.println(")");
+            return this;
+        }
     }
 }
