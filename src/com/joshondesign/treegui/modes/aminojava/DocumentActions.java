e4ddeab4cfe6e86a55a4e83aaae286607bfb8921
Josh Marinacci <joshua@marinacci.org>
2013-01-22 21:10:16 -0800

    delete bindings when the nodes are deleted

--- src/com/joshondesign/treegui/modes/aminojava/DocumentActions.java
+++ src/com/joshondesign/treegui/modes/aminojava/DocumentActions.java
@@ -1,9 +1,11 @@
 package com.joshondesign.treegui.modes.aminojava;
 
+import com.joshondesign.treegui.Binding;
 import com.joshondesign.treegui.docmodel.SketchDocument;
 import com.joshondesign.treegui.docmodel.SketchNode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class DocumentActions {
 
@@ -14,6 +16,15 @@ public class DocumentActions {
         }
         for(SketchNode child : toMove){
             document.findParent(child).remove(child);
+        }
+
+        ListIterator<Binding> it = document.getBindings().listIterator();
+        while(it.hasNext()) {
+            Binding binding = it.next();
+            if(toMove.contains(binding.getSource()) ||
+                    toMove.contains(binding.getTarget())) {
+                it.remove();
+            }
         }
         document.getSelection().clear();
     }
